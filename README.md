afcb3bdd1eb48f4f37db33215f4b55b45ba0ba8d
vidisharaichand <bca22138@glbim.ac.in>
2024-07-12 12:47:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# ResumeGen
+ReactJS Basic Resume Builder which will generate customized Resume in few minutes on your requirements.
+
+## 🖥️ Tech Stack
+
+![reactjs](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)&nbsp;
+![chakraui](https://img.shields.io/badge/Chakra--UI-319795?style=for-the-badge&logo=chakra-ui&logoColor=white)&nbsp;
+
+<h2>📬 Contact</h2>
+
+If you want to contact me, you can reach me through below handles.
+
+[![linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/vidisha-raichand-789011271/)
+
+© 2024 Vidihsa Raichand
+
---
+++ src/components/Layouts/Footer.js
@@ -0,0 +1,48 @@
+import {
+    Text,
+    Stack,
+    HStack,
+    Box,
+    Container,
+    IconButton,
+    useColorModeValue
+} from '@chakra-ui/react';
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <Box
+            bg={useColorModeValue('gray.100', 'gray.900')}
+            color={useColorModeValue('gray.700', 'gray.200')}>
+
+            <Container
+                as={Stack}
+                maxW={'7xl'}
+                py={5}
+                direction={{ base: 'column', md: 'row' }}
+                spacing={4}
+                justify={{ base: 'center', md: 'space-between' }}
+                align={{ base: 'center', md: 'center' }}>
+
+                <Stack direction={{ base: 'column', md: 'row' }} fontWeight={'medium'}>
+                    <Text>&copy; 2024 Resume Bilder. Designed By</Text>
+                    <Text as={'a'} href={'https://www.linkedin.com/in/vidisha-raichand-789011271/'} target={'_blank'} color={'blue.600'}>Vidisha Raichand</Text>
+                </Stack>
+
+
+                <Stack direction={'row'} spacing={6}>
+                    <IconButton isRound='true' as={'a'} href={'https://github.com/vidisharaichand'} target='_blank' bg={'gray.300'}>
+                        <FaGithub />
+                    </IconButton>
+                    <IconButton isRound='true' as={'a'} href={'https://www.linkedin.com/in/vidisha-raichand-789011271/'} target='_blank' colorScheme={'blue'}>
+                        <FaLinkedin />
+                    </IconButton>
+
+                </Stack>
+
+            </Container>
+        </Box>
+    )
+}
+
+export default Footer
